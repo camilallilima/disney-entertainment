3f22c326335534cdb7cacafc417adc74be69bd4b
camilallilima <camilalima.veleiros@gmail.com>
2025-04-10 16:03:11 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
-const { String } = require("core-js");
 
-(function () { 
 
-"use strict";
+(function () {
+
+  "use strict";
   /*O modo estrito ("use strict") é ativado para ajudar a detectar erros e comportamentos problemáticos no código. */
 
   /* Função de seleção */
-  
+
   const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
+      el = el.trim()
+      if (all) {
+          return [...document.querySelectorAll(el)]
+      } else {
+          return document.querySelector(el)
+      }
   }
 
   /**
    * Easy event listener function 
    * Função de Adição de Eventos (on)
    * Função espera que um evento ocorra e depois responde a ele  */
-  
-  
+
+
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
+      let selectEl = select(el, all)
+      if (selectEl) {
+          if (all) {
+              selectEl.forEach(e => e.addEventListener(type, listener))
+          } else {
+              selectEl.addEventListener(type, listener)
+          }
       }
-    }
   }
 
   // Ativar Botao Menu no modo Mobile
 
-  on('click', '.mobile-nav-toggle', function() {
-        select('body').classList.toggle('mobile-nav-active')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
+  on('click', '.mobile-nav-toggle', function () {
+      select('body').classList.toggle('mobile-nav-active')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
   })
 
-
   // Efeito Digitação
-  const typed = select('.typed');
+  const typed = select('.typed')
 
   if (typed) {
-    let typed_strings = typed.getAttibute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new typed('.typed', {
-      Strings: typed_strings
-    });
-    
+      let typed_strings = typed.getAttribute('data-typed-items')
+      typed_strings = typed_strings.split(',')
+
+      new Typed('.typed', {
+          strings: typed_strings,
+          loop: true, //deixa a palavra em loop
+          typeSpeed: 100, //velocidade de digitação
+          backSpeed: 10, // velocidade em apagar
+          backDelay: 2000, // tempo de espera
+
+      });
+
   }
-
-
-
-
-
 
 
 
